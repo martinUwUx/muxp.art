c685836394dbe8ab044466458266e82eb7f3e982
unknown <wertouyo0@gmail.com>
2026-04-29 01:26:11 -0400

    Add .gitignore and remove generated files

--- .next/dev/server/app/api/now-playing/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/now-playing/route.js")
-R.c("server/chunks/src_app_api_now-playing_route_ts_0~yl68z._.js")
-R.c("server/chunks/node_modules_next_0ck15iq._.js")
-R.c("server/chunks/[root-of-the-server]__00v260n._.js")
-R.c("server/chunks/_next-internal_server_app_api_now-playing_route_actions_0uy1db0.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/now-playing/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/now-playing/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ywI5RWOcj95n+s0g/uJBo4EEvboYPUc8BJzNsHTRa4g=\"\n}"
